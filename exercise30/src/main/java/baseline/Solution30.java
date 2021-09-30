e667522dc81683c4a30c27fc83126ee91a4a70f6
RandyRo99 <83358766+RandyRo99@users.noreply.github.com>
2021-09-30 18:44:52 -0400

    Added exercise 30 solution

--- exercise30/src/main/java/baseline/Solution30.java
+++ exercise30/src/main/java/baseline/Solution30.java
@@ -13,9 +13,38 @@ public class Solution30
 
     public static void main(String[] args)
     {
+        Solution30 solution = new Solution30();
+
         // create a double for loop
         // inside the for loop multiply each iterator by each other, to get each result
         // print a number of spaces(depending on the result) and then add the result to your print
         // drop a newline everytime the outer loop has reached its end
+
+        int multiple = 12;
+        solution.returnMultiTable(multiple);
+    }
+    private void returnMultiTable(int multiple)
+    {
+        for (int x = 1; x < multiple + 1; x++)
+        {
+            for (int y = 1; y < multiple + 1; y++)
+            {
+                int product = x * y;
+                int numSpaces = 5 - String.valueOf(product).length();
+                for (int i = 0; i < numSpaces; i++)
+                {
+                    System.out.print(" ");
+                }
+                System.out.print(product);
+            }
+            if (x == multiple)
+            {
+                continue;
+            }
+            else
+            {
+                System.out.print("\n");
+            }
+        }
     }
 }
