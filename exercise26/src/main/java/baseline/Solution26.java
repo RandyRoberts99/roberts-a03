26ea3a086511ec9e21087b4d3863aa0cd6acc28f
RandyRo99 <83358766+RandyRo99@users.noreply.github.com>
2021-09-30 03:55:33 -0400

    Readded exercise 26 w/ Pseudocode

---
+++ exercise26/src/main/java/baseline/Solution26.java
@@ -0,0 +1,21 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Randall Roberts
+ */
+
+package baseline;
+
+import java.util.Scanner;
+
+public class Solution26
+{
+    private static final Scanner reader = new Scanner(System.in);
+
+    public static void main(String[] args)
+    {
+        // Take in the balance as a double
+        // Tale in the APR as a double
+        // Take in the monthly payment as a double
+        // Output the number of months
+    }
+}
