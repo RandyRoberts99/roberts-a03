5ad839354cf7b734749532302362243b47f2abf5
RandyRo99 <83358766+RandyRo99@users.noreply.github.com>
2021-09-30 04:34:09 -0400

    Added exercise 27 Pseudocode

---
+++ exercise27/src/main/java/baseline/Solution27.java
@@ -0,0 +1,22 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Randall Roberts
+ */
+
+package baseline;
+
+import java.util.Scanner;
+
+public class Solution27
+{
+    private static final Scanner reader = new Scanner(System.in);
+
+    public static void main(String[] args)
+    {
+        // Take a first name
+        // Take a last name
+        // Take a zip code
+        // Enter the employee ID
+        // Check each string entered,
+    }
+}
