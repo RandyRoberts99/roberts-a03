e1f9565946861eac30e66a03ee92ea31e0185bb6
RandyRo99 <83358766+RandyRo99@users.noreply.github.com>
2021-09-30 02:09:35 -0400

    Readded exercise 25 with pseudocode first

---
+++ exercise25/src/main/java/baseline/Solution25.java
@@ -0,0 +1,21 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Randall Roberts
+ */
+
+package baseline;
+
+import java.util.Scanner;
+
+public class Solution25
+{
+    private static final Scanner reader = new Scanner(System.in);
+
+    public static void main(String[] args)
+    {
+        // prompt the user to enter a password
+        // determine the strength of a password through passwordValidator and returning an int that is the
+        // score of said password, 1-4
+        // create an output according to the integer
+    }
+}
