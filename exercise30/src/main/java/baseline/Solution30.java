f7736f497e26dfb6f91674a5bb5d6c6f4f8f2b30
RandyRo99 <83358766+RandyRo99@users.noreply.github.com>
2021-09-30 18:13:21 -0400

    Added pseudocode for exercise 30

---
+++ exercise30/src/main/java/baseline/Solution30.java
@@ -0,0 +1,21 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Randall Roberts
+ */
+
+package baseline;
+
+import java.util.Scanner;
+
+public class Solution30
+{
+    private static final Scanner reader = new Scanner(System.in);
+
+    public static void main(String[] args)
+    {
+        // create a double for loop
+        // inside the for loop multiply each iterator by each other, to get each result
+        // print a number of spaces(depending on the result) and then add the result to your print
+        // drop a newline everytime the outer loop has reached its end
+    }
+}
