e2e658ece54e4c54084781065ddae2a8924425ae
RandyRo99 <83358766+RandyRo99@users.noreply.github.com>
2021-10-01 18:27:28 -0400

    Added exercise 34 pseudocode

---
+++ exercise34/src/main/java/baseline/Solution34.java
@@ -0,0 +1,17 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Randall Roberts
+ */
+
+package baseline;
+
+public class Solution34
+{
+    public static void main(String[] args)
+    {
+        // create an array of predetermined size of 5.
+        // Add random names into the array
+        // ask the user to pick a name from the list
+        // delete the users input from the list, and output the new list
+    }
+}
