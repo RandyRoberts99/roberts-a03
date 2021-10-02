b8b831b3e8353fc24ea3fc9f2f78684f96f93282
RandyRo99 <83358766+RandyRo99@users.noreply.github.com>
2021-10-02 00:42:25 -0400

    Added exercise 36 solution

--- exercise36/src/main/java/baseline/Solution36.java
+++ exercise36/src/main/java/baseline/Solution36.java
@@ -33,5 +33,107 @@ public class Solution36
 
         double std = solution.getStd(values, average);
     }
-    
+    private ArrayList<Integer> fillValues(ArrayList<Integer> values)
+    {
+        boolean notDone = true;
+        int userInt;
+
+        while (notDone)
+        {
+            System.out.print("Enter a number: ");
+            String userInput = reader.next();
+            if (userInput.equalsIgnoreCase("done"))
+            {
+                notDone = false;
+            }
+            else
+            {
+                try
+                {
+                    userInt = Integer.parseInt(userInput);
+                    values.add(userInt);
+                }
+                catch (NumberFormatException e)
+                {
+                    System.out.println("Enter a valid number!");
+                }
+            }
+        }
+        return values;
+    }
+    private void printList(ArrayList<Integer> values)
+    {
+        System.out.print("Numbers: ");
+        for (int i = 0; i < values.size(); i++)
+        {
+            System.out.print(values.get(i));
+            if (i != values.size() - 1)
+            {
+                System.out.print(", ");
+            }
+        }
+        System.out.print("\n");
+    }
+    private double getAverage(ArrayList<Integer> values)
+    {
+        double fullAverage = 0;
+        for (int i = 0; i < values.size(); i++)
+        {
+            fullAverage += (double)values.get(i);
+        }
+        fullAverage /= values.size();
+        System.out.printf("The Average is %1.1f\n", fullAverage);
+        return fullAverage;
+    }
+    private int getMin(ArrayList<Integer> values)
+    {
+        int currMin = values.get(0);
+
+        for (int i = 1; i < values.size(); i++)
+        {
+            if (currMin > values.get(i))
+            {
+                currMin = values.get(i);
+            }
+        }
+        System.out.print("The minimum is " + currMin + "\n");
+
+        return currMin;
+    }
+    private int getMax(ArrayList<Integer> values)
+    {
+        int currMax = values.get(0);
+
+        for (int i = 1; i < values.size(); i++)
+        {
+            if (currMax < values.get(i))
+            {
+                currMax = values.get(i);
+            }
+        }
+        System.out.print("The maximum is " + currMax + "\n");
+
+        return currMax;
+    }
+    private double getStd(ArrayList<Integer>values, double average)
+    {
+        double sum = 0.0;
+        double standardDeviation = 0.0;
+
+        for(int i = 0; i < values.size(); i++)
+        {
+            sum += (double)(values.get(i));
+        }
+        double mean = sum/values.size();
+
+        for(int i = 0; i < values.size(); i++)
+        {
+            standardDeviation += Math.pow(((double)(values.get(i))) - mean, 2);
+        }
+        double stdDeviation = Math.sqrt(standardDeviation/values.size());
+
+        System.out.print("The standard deviation is " + String.format("%1.2f", stdDeviation));
+
+        return stdDeviation;
+    }
 }
