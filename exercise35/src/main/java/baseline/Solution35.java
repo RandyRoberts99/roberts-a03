0d2b34ff880afe0a97e163f5ff9a07f4f9ba9eeb
RandyRo99 <83358766+RandyRo99@users.noreply.github.com>
2021-10-01 21:22:17 -0400

    Added exercise 35 solution

---
+++ exercise35/src/main/java/baseline/Solution35.java
@@ -0,0 +1,50 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Randall Roberts
+ */
+
+package baseline;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Solution35
+{
+    private static final Scanner reader = new Scanner(System.in);
+    private static final Random rand = new Random();
+
+    public static void main(String[] args)
+    {
+        Solution35 solution = new Solution35();
+
+        // ask the user for inputs to fill an array until the input is blank.
+        ArrayList<String> names = new ArrayList<String>();
+
+        // take a random index from the array afterwards.
+        String winnerName = solution.getWinner(names);
+
+        // output the random index.
+        System.out.print("The winner is... " + winnerName + ".");
+    }
+    private String getWinner(ArrayList<String> names)
+    {
+        System.out.print("Enter a name: ");
+        String userInput = reader.nextLine();
+        while (userInput != "")
+        {
+            names.add(userInput);
+            System.out.print("Enter a name: ");
+            userInput = reader.nextLine();
+        }
+        String winnerName = getRandomName(names);
+        return winnerName;
+    }
+    private String getRandomName(ArrayList<String> names)
+    {
+        int i = names.size();
+        int randomWinnerIndex = rand.nextInt(i);
+        String randomWinnerName = names.get(randomWinnerIndex);
+        return randomWinnerName;
+    }
+}
