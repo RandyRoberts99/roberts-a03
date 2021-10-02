0dcf942a2239e0b5d758749c09929cb071fdc7c2
RandyRo99 <83358766+RandyRo99@users.noreply.github.com>
2021-10-02 16:56:41 -0400

    Added pseudocode to exercise 37

---
+++ exercise37/src/main/java/baseline/Solution37.java
@@ -0,0 +1,20 @@
+package baseline;
+
+import java.util.Scanner;
+
+public class Solution37
+{
+    private static final Scanner reader = new Scanner(System.in);
+
+    public static void main(String[] args)
+    {
+        //What's the minimum length? 8
+        //How many special characters? 2
+        //How many numbers? 2
+        //Your password is aurn2$1s#
+
+        // Write a program that takes in a password length, a number of special characters, and a number of numbers
+        // Use these parameters to generate a password that meets the requirements of the user
+        // output the password
+    }
+}
